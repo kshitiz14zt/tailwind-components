12bb7b236ff1204d379d4b7cc5c659bb55297a32
Kshitiz - FZT <kshitizbaral.14zt@gmail.com>
2025-12-02 09:18:30 +0545

    solve little slider and slider-button

--- src/js/slick-slider.js
+++ src/js/slick-slider.js
@@ -1,32 +1,26 @@
-$(document).ready(function () {
-    $('.slickSlider').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
+    $(document).ready(function () {
+      $('.slick-slider').slick({
         slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        infinite: true,
+        autoplay: false,
+        speed: 500,
+  
         responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            }
+          {
+            breakpoint: 1280,
+            settings: { slidesToShow: 3}
+          },
+          {
+            breakpoint: 1024,
+            settings: { slidesToShow: 2 }
+          },
+          {
+            breakpoint: 640,
+            settings: { slidesToShow: 1 }
+          }
         ]
+      });
     });
-});
